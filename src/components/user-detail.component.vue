d591696cb523889ffa957581104067f0e10536ce
Alex <smoke111@email.ru>
2019-02-27 23:06:47 +0200

    fix govnocode

--- src/components/user-detail.component.vue
+++ src/components/user-detail.component.vue
@@ -1,72 +1,72 @@
 <template>
-    <tr class="text-secondary text-left small">
-        <td colspan="3" class="align-bottom">
-            <h4>{{user.name.first | titlecase}}
-                <img style="width:40px" src="../assets/male.png" v-if="user.gender==='male'">
-                <img style="width:20px" src="../assets/female.png" v-if="user.gender==='female'">
-            </h4>
-            <div>
-                <strong>Username:</strong> {{user.login.username}}
-            </div>
-            <div>
-                <strong>Registered:</strong> {{user.registered.date | date}}
-            </div>
-            <div>
-                <strong>Email:</strong> {{user.email}}
-            </div>
-        </td>
-        <td class="align-bottom">
-            <div>
-                <strong>Address:</strong> {{user.location.street}}
-            </div>
-            <div>
-                <strong>City:</strong> {{user.location.city}}
-            </div>
-            <div>
-                <strong>Zip Code:</strong> {{user.location.postcode}}
-            </div>
-        </td>
-        <td class="align-bottom">
-            <div>
-                <strong>Birthday:</strong> {{user.dob.date | date}}
-            </div>
-            <div>
-                <strong>Phone:</strong> {{user.phone}}
-            </div>
-            <div>
-                <strong>Cell:</strong> {{user.cell}}
-            </div>
-        </td>
-        <td colspan="4" class="align-bottom text-center">
-            <img :alt="user.name.first" class="rounded-circle img-thumbnail"
-                 :src="user.picture.large" style="width:40%;">
-        </td>
-    </tr>
+  <tr class="text-secondary text-left small">
+    <td colspan="3" class="align-bottom">
+      <h4>{{user.name.first | titlecase}}
+      <img style="width:40px" src="../assets/male.png" v-if="user.gender==='male'">
+      <img style="width:20px" src="../assets/female.png" v-if="user.gender==='female'">
+      </h4>
+      <div>
+        <strong>Username:</strong> {{user.login.username}}
+      </div>
+      <div>
+        <strong>Registered:</strong> {{user.registered.date | date}}
+      </div>
+      <div>
+        <strong>Email:</strong> {{user.email}}
+      </div>
+    </td>
+    <td class="align-bottom">
+      <div>
+        <strong>Address:</strong> {{user.location.street}}
+      </div>
+      <div>
+        <strong>City:</strong> {{user.location.city}}
+      </div>
+      <div>
+        <strong>Zip Code:</strong> {{user.location.postcode}}
+      </div>
+    </td>
+    <td class="align-bottom">
+      <div>
+        <strong>Birthday:</strong> {{user.dob.date | date}}
+      </div>
+      <div>
+        <strong>Phone:</strong> {{user.phone}}
+      </div>
+      <div>
+        <strong>Cell:</strong> {{user.cell}}
+      </div>
+    </td>
+    <td colspan="4" class="align-bottom text-center">
+      <!--<img :alt="user.name.first" class="rounded-circle img-thumbnail"-->
+      <!--:src="user.picture.large" style="width:40%;">-->
+    </td>
+  </tr>
 </template>
 
 <script>
-    export default {
-        name: 'user-detail',
-        props: {
-            'user': {
-                type: Object
-            }
-        },
-        filters: {
-            titlecase(value) {
-                return value[0].toUpperCase() + value.slice(1);
-            },
-            date(value) {
-                let date = new Date(Date.parse(value));
+  export default {
+    name: 'user-detail',
+    props: {
+      user: {
+        type: Object
+      }
+    },
+    filters: {
+      titlecase(value) {
+        return value[0].toUpperCase() + value.slice(1);
+      },
+      date(value) {
+        let date = new Date(Date.parse(value));
 
-                function addZero(date) {
-                    return date < 10 ? `0${date}` : date;
-                }
+        function addZero(date) {
+          return date < 10 ? `0${date}` : date;
+        }
 
-                return addZero(date.getDate()) + "/" + addZero(date.getMonth() + 1) + "/" + date.getFullYear();
-            }
-        },
-    }
+        return addZero(date.getDate()) + "/" + addZero(date.getMonth() + 1) + "/" + date.getFullYear();
+      }
+    },
+  }
 </script>
 
 <style scoped>
--- src/components/user-list.component.vue
+++ src/components/user-list.component.vue
@@ -1,206 +1,218 @@
 <template>
-    <div class="container-fluid">
-        <template v-if="showedUsers.users.length>0">
-            <table class="table table-striped">
-                <thead>
-                <tr>
-                    <th></th>
-                    <th>Last</th>
-                    <th>First</th>
-                    <th>Username</th>
-                    <th>Phone</th>
-                    <th>Location</th>
-                    <th></th>
-                </tr>
-                </thead>
-                <tbody>
-                <template v-for="(user, index) in showedUsers.users">
-                    <tr :key="'user'+index"
-                        :class="{'active' : selectedUser === user, 'table-secondary' : index%2===0}">
-                        <td class="align-middle"><img :alt="user.name.first" class="rounded-circle img-thumbnail"
-                                                      :src="user.picture.medium"></td>
-                        <td class="align-middle">{{user.name.last | titlecase}}</td>
-                        <td class="align-middle">{{user.name.first | titlecase}}</td>
-                        <td class="align-middle">{{user.login.username}}</td>
-                        <td class="align-middle">{{user.phone}}</td>
-                        <td class="align-middle">{{user.location.state | titlecase}}</td>
-                        <td class="align-middle">
-                            <button @click='selectUser(user)' class="btn btn-outline-dark btn-control">
-                                <span>{{ selectedUser === user && selectedNow ? '-' : '+'}}</span>
-                            </button>
-                        </td>
-                    </tr>
-                    <transition name="details" :key="'user'+index">
-                        <user-detail :user="selectedUser" v-show="selectedUser === user && selectedNow"></user-detail>
-                    </transition>
-                </template>
-                </tbody>
-            </table>
-
-            <!--pagination-->
-            <nav v-if="filteredUsers.length>0">
-                <ul class="pagination pagination-md">
-                    <li v-for="data in filteredUsers" @click="showUsers(data)" :key="data.page"
-                        :class="{'active': showedUsers.page===data.page}" class="page-item"
-                        aria-current="page">
-                        <a class="page-link">{{data.page}}</a></li>
-                </ul>
-            </nav>
+  <div class="container-fluid">
+    <template v-if="showedUsers.users.length">
+      <table class="table table-striped">
+        <thead>
+        <tr>
+          <th></th>
+          <th>Last</th>
+          <th>First</th>
+          <th>Username</th>
+          <th>Phone</th>
+          <th>Location</th>
+          <th></th>
+        </tr>
+        </thead>
+        <tbody>
+        <template v-for="(user, index) in showedUsers.users">
+          <tr :key="'user' + index"
+              :class="{'active' : selectedUser === user, 'table-secondary' : index%2===0}">
+            <td class="align-middle">
+              <img :alt="user.name.first"
+                   class="rounded-circle img-thumbnail"
+                   :src="user.picture.medium"
+              >
+            </td>
+            <td class="align-middle">{{user.name.last | titlecase}}</td>
+            <td class="align-middle">{{user.name.first | titlecase}}</td>
+            <td class="align-middle">{{user.login.username}}</td>
+            <td class="align-middle">{{user.phone}}</td>
+            <td class="align-middle">{{user.location.state | titlecase}}</td>
+            <td class="align-middle">
+              <button @click='selectUser(user)' class="btn btn-outline-dark btn-control">
+                <span>{{ selectedUser === user && selectedNow ? '-' : '+'}}</span>
+              </button>
+            </td>
+          </tr>
+          <transition name="details" :key="'user'+index">
+            <user-detail :user="user" v-show="selectedUser === user && selectedNow"></user-detail>
+          </transition>
         </template>
-        <div class="text-danger" v-if="showedUsers.users.length===0 && users.length>0">Not founded users.
-            Please change your search query
-        </div>
-        <div class="alert alert-danger" v-if="errorMessage">{{ errorMessage }}</div>
+        </tbody>
+      </table>
 
-        <!--modal window-->
-        <div class="modal fade" id="chart" tabindex="-1" role="dialog" aria-labelledby="title" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="title">Gender Users</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <pie-chart v-if="users.length>0" :data="chartData"></pie-chart>
-                </div>
-            </div>
-        </div>
-        <!---->
+      <!--pagination-->
+      <nav v-if="filteredUsers.length">
+        <ul class="pagination pagination-md">
+          <li v-for="data in filteredUsers" @click="showUsers(data)" :key="data.page"
+              :class="{'active': showedUsers.page===data.page}" class="page-item"
+              aria-current="page">
+            <a class="page-link">{{data.page}}</a></li>
+        </ul>
+      </nav>
+    </template>
+    <div class="text-danger" v-if="!showedUsers.users.length && users.length">Not founded users.
+      Please change your search query
     </div>
+    <div class="alert alert-danger" v-if="errorMessage">{{ errorMessage }}</div>
+
+    <!--modal window-->
+    <div class="modal fade" id="chart" tabindex="-1" role="dialog" aria-labelledby="title" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="title">Gender Users</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <pie-chart v-if="users.length" :data="chartData"></pie-chart>
+        </div>
+      </div>
+    </div>
+    <!---->
+  </div>
 </template>
 
 <script>
-    import PieChart from "./pie-chart.component";
-    import userDetail from "./user-detail.component";
+  import PieChart from "./pie-chart.component";
+  import userDetail from "./user-detail.component";
 
-    export default {
-        name: 'user-list',
-        components: {
-            'user-detail': userDetail,
-            PieChart
+  export default {
+    name: 'user-list',
+    components: {
+      'user-detail': userDetail,
+      PieChart
+    },
+    props: {
+      search: {
+        type: String
+      }
+    },
+    filters: {
+      titlecase(value) {
+        return value[0].toUpperCase() + value.slice(1);
+      }
+    },
+    watch: {
+      search: function (value) {
+        this.filteredUsers = this.users.filter(user => user.name.first.match(value.toLowerCase()));
+        return this.searchUsers();
+      }
+    },
+    data() {
+      return {
+        url: 'https://randomuser.me/api/?results=30',
+        users: [],
+        filteredUsers: [],
+        showedUsers: {
+          page: null,
+          users: []
         },
-        props: {
-            'search': {
-                type: String
+        selectedUser: null,
+        selectedNow: false,
+        usersOnPage: 10,
+        errorMessage: '',
+        male: 0,
+        female: 0
+      }
+    },
+    methods: {
+      selectUser(user) {
+        this.selectedNow = (!(this.selectedNow && this.selectedUser === user));
+        this.selectedUser = user;
+      },
+      showUsers(data) {
+        this.showedUsers = {
+          page: data.page,
+          users: data.users
+        }
+      },
+      searchUsers(filteredUsers) {
+        console.log('filteredUsers', filteredUsers);
+        let pages = Math.ceil(this.filteredUsers.length / this.usersOnPage);
+        let users = Array(pages);
+        for (let i = 0; i < pages; i++) {
+          let numbers = i * this.usersOnPage;
+          users[i] = ({
+            page: i + 1,
+            users: this.filteredUsers.slice(numbers, numbers + this.usersOnPage)
+          });
+        }
+
+        if (users.length > 0) {
+          this.showedUsers = {
+            page: users[0].page,
+            users: users[0].users
+          }
+        } else {
+          this.showedUsers = {
+            page: null,
+            users: []
+          }
+        }
+        return users;
+      }
+    },
+    computed: {
+      chartData() {
+        return {
+          labels: ["Female", "Male"],
+          datasets: [
+            {
+              label: "Data One",
+              backgroundColor: ["pink", "lightblue"],
+              data: [this.male, this.female]
             }
-        },
-        filters: {
-            titlecase(value) {
-                return value[0].toUpperCase() + value.slice(1);
-            }
-        },
-        data() {
-            return {
-                url: 'https://randomuser.me/api/?results=30',
-                users: [],
-                showedUsers: {
-                    page: null,
-                    users: []
-                },
-                selectedUser: null,
-                selectedNow: false,
-                usersOnPage: 10,
-                errorMessage: '',
-                male: 0,
-                female: 0
-            }
-        },
-        methods: {
-            selectUser(user) {
-                this.selectedNow = (!(this.selectedNow && this.selectedUser === user));
-                this.selectedUser = user;
+          ]
+        }
+      },
+      // filteredUsers() {
+
+      // }
+    },
+    created() {
+      this.$http.get(this.url).then(response => {
+            this.users = response.body.results;
+            this.showedUsers = {
+              page: 1,
+              users: this.users.slice(0, this.usersOnPage)
             },
-            showUsers(data) {
-                this.showedUsers = {
-                    page: data.page,
-                    users: data.users
-                }
-            },
-            searchUsers(filteredUsers) {
-                let pages = Math.ceil(filteredUsers.length / this.usersOnPage);
-                let users = Array(pages);
-                for (let i = 0; i < pages; i++) {
-                    let numbers = i * this.usersOnPage;
-                    users[i] = ({
-                        page: i + 1,
-                        users: filteredUsers.slice(numbers, numbers + this.usersOnPage)
-                    });
-                }
-                if (users.length > 0) {
-                    this.showedUsers = {
-                        page: users[0].page,
-                        users: users[0].users
-                    }
-                } else {
-                    this.showedUsers = {
-                        page: null,
-                        users: []
-                    }
-                }
-                return users;
-            }
-        },
-        computed: {
-            chartData() {
-                return {
-                    labels: ["Female", "Male"],
-                    datasets: [
-                        {
-                            label: "Data One",
-                            backgroundColor: ["pink", "lightblue"],
-                            data: [this.male, this.female]
-                        }
-                    ]
-                }
-            },
-            filteredUsers() {
-                let filteredUsers = this.users.filter(user => user.name.first.match(this.search.toLowerCase()));
-                return this.searchUsers(filteredUsers);
-            }
-        },
-        created() {
-            this.$http.get(this.url).then(response => {
-                    this.users = response.body.results;
-                    this.showedUsers = {
-                        page: 1,
-                        users: this.users.slice(0, this.usersOnPage)
-                    },
-                        this.users.forEach(user => user.gender === 'male' ? this.male++ : this.female++);
-                },
-                error => this.errorMessage = error.body.error);
-        },
-    }
+                this.users.forEach(user => user.gender === 'male' ? this.male++ : this.female++);
+          },
+          error => this.errorMessage = error.body.error);
+    },
+  }
 </script>
 
 <style scoped>
-    .btn-outline-dark {
-        border: none !important;
-        font-weight: bold;
-        font-size: 50px;
-    }
+  .btn-outline-dark {
+    border: none !important;
+    font-weight: bold;
+    font-size: 50px;
+  }
 
-    .details-enter-active, .details-leave-active {
-        transition: all ease .5s;
-    }
+  .details-enter-active, .details-leave-active {
+    transition: all ease .5s;
+  }
 
-    .details-enter {
-        transform: translateY(50px);
-        opacity: 0;
-    }
+  .details-enter {
+    transform: translateY(50px);
+    opacity: 0;
+  }
 
-    .details-leave-to {
-        transform: translateY(-50px);
-        opacity: 0;
-    }
+  .details-leave-to {
+    transform: translateY(-50px);
+    opacity: 0;
+  }
 
-    .btn-control {
-        width: 80px;
-        height: 80px;
-    }
+  .btn-control {
+    width: 80px;
+    height: 80px;
+  }
 
-    .page-item {
-        cursor: pointer;
-    }
+  .page-item {
+    cursor: pointer;
+  }
 
 </style>
